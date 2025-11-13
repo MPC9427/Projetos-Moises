97c6f07d4a60c77cf74ea44180f86b80e4393cad
Moises Piva da Costa <moisespiva9492@gmail.com>
2025-11-13 02:42:23 -0300

    Update

--- funcs.js
+++ funcs.js
@@ -1,18 +1,18 @@
-function valida() {
-    if (form['usu'].value == "") {
-        confirm("O campo USUARIO e obrigatorio");
-        form['usu'].focus();
-        return false
+    function valida() {
+        if (form['usu'].value == "") {
+            confirm("O campo USUARIO e obrigatorio");
+            form['usu'].focus();
+            return false
+        }
+        if (form['senha'].value == "") {
+            alert("O campo SENHA e obrigatorio");
+            form['senha'].focus();
+            return false
+        }
     }
-    if (form['senha'].value == "") {
-        alert("O campo SENHA e obrigatorio");
-        form['senha'].focus();
-        return false
-    }
-}
 
 
- const botao = document.getElementById("toggleDarkMode");
+    const botao = document.getElementById("toggleDarkMode");
 
       botao.addEventListener("click", function () {
        document.body.classList.toggle("dark-mode");
@@ -25,39 +25,40 @@ function valida() {
                }
         });
 									
-
-        function updateTime() {
-					var now = new Date();
-					var hours = now.getHours().toString().padStart(2, '0');
-					var minutes = now.getMinutes().toString().padStart(2, '0');
-					var seconds = now.getSeconds().toString().padStart(2, '0');
-					var timeString = hours + ':' + minutes + ':' + seconds;
-					document.getElementById('clock').textContent = timeString;
+ 
+    function updateTime() {
+	    var now = new Date();
+		var hours = now.getHours().toString().padStart(2, '0');
+		var minutes = now.getMinutes().toString().padStart(2, '0');
+		var seconds = now.getSeconds().toString().padStart(2, '0');
+		var timeString = hours + ':' + minutes + ':' + seconds;
+		document.getElementById('clock').textContent = timeString;
 				}
-				setInterval(updateTime, 1000);
-				updateTime();
+			setInterval(updateTime, 1000);
+			updateTime();
 
-				function alternarSenha() {
-				const campoSenha = document.getElementById("senha");
-				campoSenha.type = campoSenha.type === "password" ? "text" : "password";
-				}
+    function alternarSenha() {
+		const campoSenha = document.getElementById("senha");
+		campoSenha.type = campoSenha.type === "password" ? "text" : "password";
+			}
 
 
-			function recuperarSenha(email) {
-				sendPasswordResetEmail(auth, email)
-				.then(() => alert("Email de recuperação enviado!"))
-				.catch((error) => alert("Erro: " + error.message));
+    function recuperarSenha(email) {
+		sendPasswordResetEmail(auth, email)
+		.then(() => alert("Email de recuperação enviado!"))
+		.catch((error) => alert("Erro: " + error.message));
 			
 				}
 
-			function valida() {
-				const usu = document.form.usu.value.trim();
-				const senha = document.form.senha.value.trim();
-				const alerta = document.querySelector('.msg_alerta');
+    function valida() {
+		const usu = document.form.usu.value.trim();
+		const senha = document.form.senha.value.trim();
+		const alerta = document.querySelector('.msg_alerta');
 
-				if (!usu || !senha) {
-						alerta.style.display = 'block';
-						return false;
+			if (!usu || !senha) {
+				alerta.style.display = 'block';
+				return false;
+                
 				}
 
 				alerta.style.display = 'none';
